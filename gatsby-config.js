8f17a314fa2258cb057f765e535c7bec6399aece
Ricardo Chavez <rchavez@devosinc.com>
2018-05-06 03:28:20 -0500

    Acerca de y feed RSS

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,60 @@
 module.exports = {
     siteMetadata: {
-        title: 'Friday Songbird'
+        title: 'Friday Songbird',
+        description: 'Todos los viernes una canción',
+        siteUrl: 'http://www.ctrl-c.club/~ricardo/friday-songbird'
     },
     pathPrefix: '/~ricardo/friday-songbird',
     plugins: [
-        'gatsby-plugin-react-helmet'
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-plugin-feed',
+            options: {
+                query: `
+                    {
+                        site {
+                            siteMetadata {
+                                title
+                                description
+                                siteUrl
+                                site_url: siteUrl
+                            }
+                        }
+                    }
+                `,
+                feeds: [
+                    {
+                        serialize: ({query: {site, allPost}}) => {
+                            return allPost.edges.map(({node}) => {
+                                return {
+                                    title: node.titulo,
+                                    date: node.fecha_publicacion,
+                                    description: node.contenidoHTML,
+                                    url: site.siteMetadata.siteUrl + node.slug,
+                                    guid: site.siteMetadata.siteUrl + node.slug,
+                                    custom_elements: [{"content:encoded": node.contenidoHTML}]
+                                };
+                            });
+                        },
+                        query: `{
+                            allPost(sort: {fields: [fecha_publicacion], order: DESC}) {
+                                edges {
+                                    node {
+                                        titulo
+                                        artista
+                                        album
+                                        contenidoHTML
+                                        fecha_publicacion
+                                        spotify_uri
+                                        slug
+                                    }
+                                }
+                            }
+                        }`,
+                        output: '/rss.xml'
+                    }
+                ]
+            }
+        }
     ]
 };
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import songbirdPic from './songbird.jpg'
+import songbirdPic from './logo_songbird.jpg'
 
 const Sidebar = ({ siteTitle }) => (
   <div className="sidebar">
@@ -16,7 +16,10 @@ const Sidebar = ({ siteTitle }) => (
                   }} />
               </div>
               <h1><Link to="/">{siteTitle}</Link></h1>
-              <p>Todos los viernes, una canción.<br/>Un proyecto de @ricardochavezt.</p>
+              <p>Todos los viernes, una canción.<br/><Link to="/about">Más...</Link></p>
+              <p>
+                  Un proyecto de <a href="https://twitter.com/ricardochavezt">@ricardochavezt</a>.
+              </p>
               <p><Link to="/archive">Archivo de canciones</Link></p>
           </div>
       </div>
